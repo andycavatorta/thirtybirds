ecf8761b7a5f5372ce39302f64dc0243cb3c8dcd
andycavatorta <andycavatorta@gmail.com>
2016-09-07 21:07:53 -0400

    finished Updates code. Starting testing

--- Logs/main.py
+++ Logs/main.py
@@ -1,3 +1,24 @@
+"""
+peraps go back to passing logging function to other modules
+use Queue object to make thread safe
+create non-trace decorator for efficiency
+how to collect data from other devices?
+    all devices send log data to the server
+
+
+new optional command-line args
+    nodename= (overrides local hostname)
+    loglevel=trace  ( default is no trace)
+    dashboard=true (add role of collecting logs)
+
+internal data formats:
+    topic (single word) - defines a flow/shape of data, not its function
+    address (path, possibly RESTful) - represents not a final address within the software, but an ontology in the data
+    data (hash type) - the payload data
+
+    does the action
+
+"""
 
 class ExceptionCollector(object):
     def __init__(self, path, errors = [], errorreturn = None):
@@ -19,4 +40,75 @@ class ExceptionCollector(object):
                 self.report("error", E, traceback.print_exc())
                 return self.errorreturn
         return returnfunction
+
+
+class ExceptionCollector_Trace(object):
+    def __init__(self, path, errors = [], errorreturn = None):
+        self.errors = errors
+        self.errorreturn = errorreturn
+        self.path = path
+
+    def report(self, level, msg, stacktrace=None):
+        print level, self.path, msg, stacktrace
+
+    def __call__(self, function):
+        def returnfunction(*args, **kwargs):
+            try:
+                self.report("trace", "starting", None)
+                return function(*args, **kwargs)
+            except Exception as E:
+                if type(E) not in self.errors:
+                    raise E
+                self.report("error", E, traceback.print_exc())
+                return self.errorreturn
+        return returnfunction
+
+
+class ExceptionCollector_Info(object):
+    def __init__(self, path, errors = [], errorreturn = None):
+        self.errors = errors
+        self.errorreturn = errorreturn
+        self.path = path
+
+    def report(self, level, msg, stacktrace=None):
+        print level, self.path, msg, stacktrace
+
+    def __call__(self, function):
+        def returnfunction(*args, **kwargs):
+            try:
+                self.report("trace", "starting", None)
+                return function(*args, **kwargs)
+            except Exception as E:
+                if type(E) not in self.errors:
+                    raise E
+                self.report("error", E, traceback.print_exc())
+                return self.errorreturn
+        return returnfunction
+
+
+class ExceptionCollector_Quiet(object):
+    def __init__(self, path, errors = [], errorreturn = None):
+        self.errors = errors
+        self.errorreturn = errorreturn
+        self.path = path
+
+    def report(self, level, msg, stacktrace=None):
+        print level, self.path, msg, stacktrace
+
+    def __call__(self, function):
+        def returnfunction(*args, **kwargs):
+            try:
+                self.report("trace", "starting", None)
+                return function(*args, **kwargs)
+            except Exception as E:
+                if type(E) not in self.errors:
+                    raise E
+                self.report("error", E, traceback.print_exc())
+                return self.errorreturn
+        return returnfunction
+
+
+
+def init(loglevel="error", mode="client"):
+    pass
 
---
+++ Updates/manager.py
@@ -0,0 +1,73 @@
+import commands
+import json
+import os
+import pickle
+import sys
+
+import upgradeScripts
+
+
+
+class Updates():
+    def __init__(self, local_path, runGithubSync = False, runBashScripts = False):
+        self.local_path = local_path
+        self.network_path = os.path.dirname(os.path.realpath(__file__))
+        self.version_pickle_path = "%s/%s" % (self.network_path, "version.pickle")
+        self.default_version_number = 0.0
+        print self.local_path, self.network_path, self.version_pickle_path
+
+    def run_bash_command(self, cmd):
+        status, output = commands.getstatusoutput(cmd)
+        return (status, output)
+        # log any errors
+
+    def run_github_sync(self):
+        cmd = "cd %s && git pull -q --all -p" % (self.local_path)
+        run_bash_command(cmd)
+
+    def make_version_pickle(self):
+        if not os.path.isfile(self.version_pickle_path):
+            pfile = open(self.version_pickle_path, "wb")
+            pickle.dump(self.default_version_number, pfile)
+            return True
+        return False
+
+    def read_version_pickle(self):
+        if not os.path.isfile(self.version_pickle_path):
+            self.make_version_pickle()
+            return self.default_version_number
+        return pickle.load(open(self.version_pickle_path, "r"))
+
+    def write_version_pickle(self, version):
+        if not os.path.isfile(self.version_pickle_path):
+            self.make_version_pickle()
+            return self.default_version_number
+        pickle.dump(float(version),open(self.version_pickle_path, "wb"))
+
+    def reset_version_pickle(self):
+        if not os.path.isfile(self.version_pickle_path):
+            self.make_version_pickle()
+        pfile = open(self.version_pickle_path, "wb")
+        pickle.dump(self.default_version_number, pfile)
+
+    def run_uprade_scripts(self):
+        import upgradeScripts
+        v_l =  upgradeScripts.scripts.keys()
+        v_l.sort(key=float)
+        output = []
+        for version in v_l:
+            if float(version) >= versionFromPickle:
+                for script in upgradeScripts.scripts[version]:
+                    status, output = self.run_bash_command(script)
+                    output.append(version, status, output)
+        return output
+
+def init(local_path, runGithubSync = False, runBashScripts = False):
+    updates = Updates(local_path, runGithubSync, runBashScripts)
+    ghStatus = ""
+    bsStatus = []
+    if runGithubSync:
+        ghStatus = updates.run_github_sync()
+    if runBashScripts:
+        bhStatus = updates.run_uprade_scripts()
+    return (updates, ghStatus, bsStatus)
