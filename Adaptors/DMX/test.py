abe43a1a1cb6e5f05ae7252faf090cfb177d05c9
andycavatorta <andycavatorta@gmail.com>
2016-09-26 16:04:44 +0200

    debugging discovery problem

--- Adaptors/DMX/test.py
+++ Adaptors/DMX/test.py
@@ -11,14 +11,14 @@ while True:
     for val in range(255):
         print "value=", val
         for ch in range (40):
-            if ch != 8:
+            #if ch != 8:
                 dmx.set(ch, val)
         #dmx.set(8, 0)
         time.sleep(0.02)
     for val in range(255):
         print "value=", val
         for ch in range (40):
-            if ch != 8:
+            #if ch != 8:
                 dmx.set(ch, 255-val)
         #dmx.set(8, 0)
         time.sleep(0.02)
