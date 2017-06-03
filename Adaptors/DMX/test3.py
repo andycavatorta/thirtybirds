45122f9354c051d66a72230d933447534bc49483
Andy Cavatorta <andycavatorta@gmail.com>
2017-06-03 12:12:48 +0200

    adding debug to DMX

--- Adaptors/DMX/test3.py
+++ Adaptors/DMX/test3.py
@@ -1,36 +1,14 @@
-"""
-1 pump
-4 pump
-5 
-6
-7
-9
-
-
-"""
 
 import time
 import threading
 
-#import simpledmx
-
-"""
-dmx = simpledmx.DMXConnection("/dev/serial/by-id/usb-ENTTEC_DMX_USB_PRO_EN211488-if00-port0")
-while True:
-    dmx.setChannel(1, 255, True)
-    time.sleep(60)
-    dmx.setChannel(1, 0, True)
-    time.sleep(60)
-
-"""
-
 from main import init as dmx_init
 dmx = dmx_init(universe=0,frame_size=40)
 
-while True:
-    dmx.set(1, 255)
-    dmx.set(2, 255)
-    time.sleep(60)
-    dmx.set(1, 0)
-    dmx.set(2, 0)
-    time.sleep(60)
+def test_cycle(channels,values,delay):
+    for value in values:
+        for channel in channels:
+            print channel,value
+            dmx.set(channel,value)
+            time.sleep(0.01)
+        time.sleep(delay)
