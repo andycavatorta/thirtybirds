054429440029cf48bf81a91d9fc297b702a78266
Andy Cavatorta <andycavatorta@gmail.com>
2017-06-03 21:46:28 +0200

    dmx debug

--- Adaptors/DMX/test3.py
+++ Adaptors/DMX/test3.py
@@ -3,16 +3,14 @@ import time
 import threading
 
 import simpledmx
-simpledmx.DMXConnection("/dev/ttyUSB0")
+dmx = simpledmx.DMXConnection("/dev/ttyUSB0")
 
-from main3 import init as dmx_init
-dmx = dmx_init(universe=0,frame_size=40)
 
 def test_cycle(channels,values,delay):
     for value in values:
         for channel in channels:
             print channel,value
-            dmx.set(channel,value)
+            dmx.setChannel(channel,value, True)
             time.sleep(0.01)
         time.sleep(delay)
 
